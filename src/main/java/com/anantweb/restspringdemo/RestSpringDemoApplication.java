1c1fa7f30ac08b23a41ad3ca4bd07f7ac4663d05
Anant Dargude <anantdargude@gmail.com>
2024-10-13 09:17:36 +0530

    Check

---
+++ src/main/java/com/anantweb/restspringdemo/RestSpringDemoApplication.java
@@ -0,0 +1,15 @@
+package com.anantweb.restspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringDemoApplication.class, args);
+
+		System.out.println("\n<<<<<----- Spring App Started ----->>>>>");
+	}
+
+}
---
+++ src/test/java/com/anantweb/restspringdemo/RestSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anantweb.restspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
